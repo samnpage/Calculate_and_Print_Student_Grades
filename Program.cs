d73b7de59c9d5775602e3f58e6fe764025206afc
Sam Page <samnpage96@gmail.com>
2024-09-23 16:43:27 -0400

    Updated console to display in a table format

--- Program.cs
+++ Program.cs
@@ -35,7 +35,8 @@ decimal nicolasScore = (decimal) nicolasSum / currentAssignments;
 decimal zahirahScore = (decimal) zahirahSum / currentAssignments;
 decimal jeongScore = (decimal) jeongSum / currentAssignments;
 
-Console.WriteLine("Sophia: " + sophiaScore + " A");
-Console.WriteLine("Nicolas: " + nicolasScore + " B");
-Console.WriteLine("Zahirah: " + zahirahScore + " B");
-Console.WriteLine("Jeong: " + jeongScore + " A");
+Console.WriteLine("Student\tGrade\n");
+Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA");
+Console.WriteLine("Nicolas:\t" + nicolasScore + "\tB");
+Console.WriteLine("Zahirah:\t" + zahirahScore + "\tB");
+Console.WriteLine("Jeong:\t\t" + jeongScore + "\tA");
